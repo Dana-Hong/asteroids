ea36c09e5b64d383b9f945adad2da2028873e369
Dana Hong <77565946+Dana-Hong@users.noreply.github.com>
2025-02-01 21:05:22 +0000

    Refactor main.py to include groups

--- main.py
+++ main.py
@@ -12,19 +12,28 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill((0, 0, 0))
-        player.draw(screen)
+
+        updatable.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
-
-        player.update(dt)
 
 
 if __name__ == "__main__":
